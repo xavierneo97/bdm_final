3aa3b31c2be66a1cd83024dc367c53bb5144b099
xavierneo97 <98359556+xavierneo97@users.noreply.github.com>
2022-05-17 03:24:18 -0400

    Add files via upload

--- BDM_Final_EC1_xqn1.py
+++ BDM_Final_EC1_xqn1.py
@@ -12,79 +12,96 @@ def main(sc):
   import pyproj
   from pyproj import Transformer
   from shapely.geometry import Point
+  from itertools import chain
 
   spark = SparkSession(sc)
 
-  # READ MAIN DATASET
-
-  df = spark.read.csv('/tmp/bdm/weekly-patterns-nyc-2019-2020/*',
-                    header=True,
-                    escape='\"') \
-                    .select('placekey','poi_cbg','visitor_home_cbgs','date_range_start','date_range_end') \
-                    .cache()
-
-
- # STEP 1: FILTER MAIN DATAFRAME FOR VISITS TO NYC SUPERMARKETS USING nyc_supermarkets.csv
+  # STEP 1: FILTER MAIN DATAFRAME WITH nyc_supermarkets.csv
 
   # Read nyc_supermarkets.csv and take columns of interest: safegraph_placekey (to match to main dataset), latitude and longitude
   df_supermarkets = spark.read.csv('nyc_supermarkets.csv', header=True) \
-        .select('safegraph_placekey',
-        F.col('latitude').alias('supermarkets_x').cast('float'),
-        F.col('longitude').alias('supermarkets_y').cast('float')) \
-        .cache()
+    .select('safegraph_placekey',
+          F.col('latitude').alias('supermarkets_x').cast('float'),
+          F.col('longitude').alias('supermarkets_y').cast('float')) \
+          .na.drop() \
+          .cache()
 
-  # Inner join and filter main dataframe with df_supermarkets on placekeys
-  df2 = df.join(df_supermarkets, df['placekey']==df_supermarkets['safegraph_placekey'], how='inner').cache()
+  set_supermarkets = set(df_supermarkets.select('safegraph_placekey').distinct().rdd.flatMap(lambda x: x).collect())
+
+  # Filter main dataframe with df_supermarkets on placekeys
+
+  df = spark.read.csv('/tmp/bdm/weekly-patterns-nyc-2019-2020/*',
+                      header=True,
+                      escape='\"') \
+                      .select('placekey','poi_cbg','visitor_home_cbgs','date_range_start','date_range_end') \
+                      .na.drop() \
+                      .where(F.col('placekey').isin(set_supermarkets)) \
+                      .cache()
+
 
 
   # STEP 2: FILTER FOR VISITS OVERLAPPING WITH 4 MONTHS OF INTEREST
-  dates = {'2019-03','2019-10','2020-03','2020-10'}
 
-  # Extract months from date_range_start and date_range_end
-  df2 = df2.withColumn('start',df.date_range_start.substr(1,7)) \
-         .withColumn('end',df.date_range_end.substr(1,7)) \
-         .where(F.col('start').isin(dates) | F.col('end').isin(dates))
+  # Filter dataset for either start or end overlapping with 4 months of interest
+  dates = {'2019-03','2019-10','2020-03','2020-10'}
 
   # Create udf to retain only one date from both columns
   udf_dates = F.udf(lambda x,y: x if x in dates else y)
-  df3 = df2.withColumn('date', udf_dates(F.col('start'),F.col('end'))) \
-    .select('placekey','poi_cbg','visitor_home_cbgs','date','supermarkets_x','supermarkets_y') \
+
+  # Extract months from date_range_start and date_range_end
+  df2 = df.withColumn('start',df.date_range_start.substr(1,7)) \
+    .withColumn('end',df.date_range_end.substr(1,7)) \
+    .where(F.col('start').isin(dates) | F.col('end').isin(dates)) \
+    .withColumn('date', udf_dates(F.col('start'),F.col('end'))) \
+    .select('placekey','poi_cbg','visitor_home_cbgs','date') \
     .cache()
 
 
 
   # STEP 3: FILTER FOR VISITS IN nyc_cbg_centroids.csv
 
-  # Explode json inside visitor_home_cbgs column to multiple rows
+  # Read nyc_cbg_centroids.csv
+  df_cbgs = spark.read.csv('nyc_cbg_centroids.csv', header=True) \
+    .select('cbg_fips',
+          F.col('latitude').alias('cbg_x').cast('float'),
+          F.col('longitude').alias('cbg_y').cast('float')) \
+          .na.drop() \
+          .cache()
+
+  set_cbgs = set(df_cbgs.select('cbg_fips').distinct().rdd.flatMap(lambda x: x).collect())
+
+
+  # Explode json inside visitor_home_cbgs column into multiple rows
   # Using method from FAQ section of Safegraph Docs
+  # Filter visitor_home_cbgs with nyc_cbg_centroids
+
+  import json
 
   def parser(element):
     return json.loads(element)
 
   jsonudf = F.udf(parser, MapType(StringType(), IntegerType()))
 
-  visitor_home_cbgs_parsed = df3.withColumn('parsed_visitor_home_cbgs', jsonudf('visitor_home_cbgs'))
-  visitor_home_cbgs_exploded = visitor_home_cbgs_parsed.select('placekey','poi_cbg', 'date', 'supermarkets_x','supermarkets_y', F.explode('parsed_visitor_home_cbgs'))
-
- # Read nyc_cbg_centroids.csv and cast latitude/longitude to floats
-  df_cbgs = spark.read.csv('nyc_cbg_centroids.csv', header=True) \
-                      .select('cbg_fips',
-        F.col('latitude').alias('cbg_x').cast('float'),
-        F.col('longitude').alias('cbg_y').cast('float'))
-
-  # Join main dataframe with df_cbgs on cbg_fips
-  df4 = visitor_home_cbgs_exploded.join(df_cbgs, visitor_home_cbgs_exploded['key']==df_cbgs['cbg_fips'], how='inner').cache()
-
-  df4 = df4.select('cbg_fips','date',
-                  df4['value'].alias('visitor_count'),
-                  'supermarkets_x','supermarkets_y','cbg_x','cbg_y') \
-                  .cache()
+  visitor_home_cbgs_parsed = df2.withColumn('parsed_visitor_home_cbgs', jsonudf('visitor_home_cbgs')).cache()
+  df3 = visitor_home_cbgs_parsed.select('placekey','poi_cbg', 'date', F.explode('parsed_visitor_home_cbgs')) \
+    .groupBy('placekey','poi_cbg','date','key').sum('value') \
+    .where(F.col('key').isin(set_cbgs)) \
+    .cache()
 
 
-  # STEP 4: REPROJECT COORDINATES AND CALCULATE DISTANCE
+
+
+  # STEP 4: CREATE A DF OF UNIQUE ORIGIN-DESTINATION PAIRS TO CALCULATE DISTANCES ONCE PER PAIR
+
+  # Concat origin-destination columns
+  def concat(x,y):
+    o_d = str(x)+'_'+str(y)
+    return str(o_d)
+
+  merge_od = F.udf(concat)
+ 
   # Define function for transforming to 2263 and calculating distance
   def obtain_distance(x, y, a, b):
-
     t = Transformer.from_crs(4326,2263)
     new_x, new_y = t.transform(x,y)
     new_a, new_b = t.transform(a,b)
@@ -94,12 +111,39 @@ def main(sc):
   # Create udf
   udf_dist = F.udf(obtain_distance)
 
- # Calculate distances
-  df5 = df4.withColumn('dist', udf_dist(F.col('supermarkets_x'), F.col('supermarkets_y'), F.col('cbg_x'), F.col('cbg_y'))) \
-  .select('cbg_fips','date','visitor_count',
-          F.col('dist').cast(FloatType())).cache()
+  # Calculate distances using df of unique origin-destination pairs
+  dist_df = df3.withColumn('origin-dest', merge_od(F.col('placekey'), F.col('key'))) \
+    .select('origin-dest', 'placekey', 'key') \
+    .distinct() \
+    .join(df_supermarkets, F.col('placekey')==df_supermarkets['safegraph_placekey'], how='inner') \
+    .join(df_cbgs, F.col('key')==df_cbgs['cbg_fips'], how='inner') \
+    .withColumn('dist', udf_dist(F.col('supermarkets_x'), F.col('supermarkets_y'), F.col('cbg_x'), F.col('cbg_y'))) \
+    .select('origin-dest','dist') \
+    .cache()
+
+  # Create dictionary of distances per origin-destination pair
+  # Use dictionary to map original df
+
+  dist_dict = {x['origin-dest']: x['dist'] for x in dist_df.collect()}
+  mapping_expr = F.create_map([F.lit(x) for x in chain(*dist_dict.items())])
 
 
+
+  # STEP 5: GET AVERAGE DISTANCE AND FINAL OUTPUT
+
+  # Get total visitor count and distance per cbg
+  # Compute average distance per cbg
+  # Pivot by date
+  # Sort by cbg_fips
+
+  df4 = df3.withColumn('origin-dest', merge_od(F.col('placekey'), F.col('key'))) \
+    .withColumn('dist', mapping_expr.getItem(F.col('origin-dest'))) \
+    .na.drop() \
+    .select(F.col('key').alias('cbg_fips'),
+            'date',
+            F.col('sum(value)').alias('visitor_count'),
+            F.col('dist').cast(FloatType())) \
+            .cache()
 
   # EXTRA CREDIT 1: CALCULATE MEDIAN
 
@@ -111,7 +155,7 @@ def main(sc):
 
   n_to_array = F.udf(lambda x: [x] * x, ArrayType(IntegerType()))
 
-  df5a = df5.withColumn('n', n_to_array(df5.visitor_count)) \
+  df5 = df4.withColumn('n', n_to_array(df4.visitor_count)) \
          .withColumn('n', F.explode(F.col('n'))) \
     .groupBy('cbg_fips','date') \
     .agg(F.expr('percentile(dist, array(0.5))')[0].alias('median_dist')) \
--- BDM_Final_EC2_xqn1.py
+++ BDM_Final_EC2_xqn1.py
@@ -14,82 +14,99 @@ def main(sc):
   import numpy as np
   from pyproj import Transformer
   from shapely.geometry import Point
+  from itertools import chain
 
   spark = SparkSession(sc)
 
-  # READ MAIN DATASET
-
-  df = spark.read.csv('/tmp/bdm/weekly-patterns-nyc-2019-2020/*',
-                    header=True,
-                    escape='\"') \
-                    .select('placekey','poi_cbg','visitor_home_cbgs','date_range_start','date_range_end') \
-                    .cache()
-
-
-
-  # STEP 1: FILTER MAIN DATAFRAME FOR VISITS TO NYC SUPERMARKETS USING nyc_supermarkets.csv
+  # EXTRA CREDIT 2
+  # STEP 1: FILTER MAIN DATAFRAME FOR VISITS TO NYC SUPERMARKETS USING core-places-nyc.csv
 
   # Read core-places-nyc and filter for places with naics_code starting with 4451
-  df_supermarkets = spark.read.csv('/tmp/bdm/core-places-nyc/*', header=True) \
-        .select('placekey',
+  df_supermarkets = spark.read.csv('/tmp/bdm/core-places-nyc.csv', header=True) \
+        .select(F.col('placekey').alias('naics_placekey'),
         F.col('naics_code').cast(StringType()),
         F.col('latitude').alias('supermarkets_x').cast('float'),
         F.col('longitude').alias('supermarkets_y').cast('float')) \
-        .where(F.col('naics_subset').startswith('4451')) \
+        .na.drop() \
+        .where(F.col('naics_code').startswith('4451')) \
         .cache()
 
-  # Inner join and filter main dataframe with df_supermarkets on placekeys
-  df2 = df.join(df_supermarkets, df['placekey']==df_supermarkets['safegraph_placekey'], how='inner').cache()
+  set_supermarkets = set(df_supermarkets.select('naics_placekey').distinct().rdd.flatMap(lambda x: x).collect())
+
+  # Filter main dataframe with df_supermarkets on placekeys
+
+  df = spark.read.csv('/tmp/bdm/weekly-patterns-nyc-2019-2020/*',
+                      header=True,
+                      escape='\"') \
+                      .select('placekey','poi_cbg','visitor_home_cbgs','date_range_start','date_range_end') \
+                      .na.drop() \
+                      .where(F.col('placekey').isin(set_supermarkets)) \
+                      .cache()
+
 
 
   # STEP 2: FILTER FOR VISITS OVERLAPPING WITH 4 MONTHS OF INTEREST
-  dates = {'2019-03','2019-10','2020-03','2020-10'}
 
-  # Extract months from date_range_start and date_range_end
-  df2 = df2.withColumn('start',df.date_range_start.substr(1,7)) \
-         .withColumn('end',df.date_range_end.substr(1,7)) \
-         .where(F.col('start').isin(dates) | F.col('end').isin(dates))
+  # Filter dataset for either start or end overlapping with 4 months of interest
+  dates = {'2019-03','2019-10','2020-03','2020-10'}
 
   # Create udf to retain only one date from both columns
   udf_dates = F.udf(lambda x,y: x if x in dates else y)
-  df3 = df2.withColumn('date', udf_dates(F.col('start'),F.col('end'))) \
-    .select('placekey','poi_cbg','visitor_home_cbgs','date','supermarkets_x','supermarkets_y') \
+
+  # Extract months from date_range_start and date_range_end
+  df2 = df.withColumn('start',df.date_range_start.substr(1,7)) \
+    .withColumn('end',df.date_range_end.substr(1,7)) \
+    .where(F.col('start').isin(dates) | F.col('end').isin(dates)) \
+    .withColumn('date', udf_dates(F.col('start'),F.col('end'))) \
+    .select('placekey','poi_cbg','visitor_home_cbgs','date') \
     .cache()
 
 
 
   # STEP 3: FILTER FOR VISITS IN nyc_cbg_centroids.csv
 
-  # Explode json inside visitor_home_cbgs column to multiple rows
+  # Read nyc_cbg_centroids.csv
+  df_cbgs = spark.read.csv('nyc_cbg_centroids.csv', header=True) \
+    .select('cbg_fips',
+          F.col('latitude').alias('cbg_x').cast('float'),
+          F.col('longitude').alias('cbg_y').cast('float')) \
+          .na.drop() \
+          .cache()
+
+  set_cbgs = set(df_cbgs.select('cbg_fips').distinct().rdd.flatMap(lambda x: x).collect())
+
+
+  # Explode json inside visitor_home_cbgs column into multiple rows
   # Using method from FAQ section of Safegraph Docs
+  # Filter visitor_home_cbgs with nyc_cbg_centroids
+
+  import json
 
   def parser(element):
     return json.loads(element)
 
   jsonudf = F.udf(parser, MapType(StringType(), IntegerType()))
 
-  visitor_home_cbgs_parsed = df3.withColumn('parsed_visitor_home_cbgs', jsonudf('visitor_home_cbgs'))
-  visitor_home_cbgs_exploded = visitor_home_cbgs_parsed.select('placekey','poi_cbg', 'date', 'supermarkets_x','supermarkets_y', F.explode('parsed_visitor_home_cbgs'))
-
-  # Read nyc_cbg_centroids.csv and cast latitude/longitude to floats
-  df_cbgs = spark.read.csv('nyc_cbg_centroids.csv', header=True) \
-                      .select('cbg_fips',
-        F.col('latitude').alias('cbg_x').cast('float'),
-        F.col('longitude').alias('cbg_y').cast('float'))
-
-  # Join main dataframe with df_cbgs on cbg_fips
-  df4 = visitor_home_cbgs_exploded.join(df_cbgs, visitor_home_cbgs_exploded['key']==df_cbgs['cbg_fips'], how='inner').cache()
-
-  df4 = df4.select('cbg_fips','date',
-                  df4['value'].alias('visitor_count'),
-                  'supermarkets_x','supermarkets_y','cbg_x','cbg_y') \
-                  .cache()
+  visitor_home_cbgs_parsed = df2.withColumn('parsed_visitor_home_cbgs', jsonudf('visitor_home_cbgs')).cache()
+  df3 = visitor_home_cbgs_parsed.select('placekey','poi_cbg', 'date', F.explode('parsed_visitor_home_cbgs')) \
+    .groupBy('placekey','poi_cbg','date','key').sum('value') \
+    .where(F.col('key').isin(set_cbgs)) \
+    .cache()
 
 
-  # STEP 4: REPROJECT COORDINATES AND CALCULATE DISTANCE
+
+
+  # STEP 4: CREATE A DF OF UNIQUE ORIGIN-DESTINATION PAIRS TO CALCULATE DISTANCES ONCE PER PAIR
+
+  # Concat origin-destination columns
+  def concat(x,y):
+    o_d = str(x)+'_'+str(y)
+    return str(o_d)
+
+  merge_od = F.udf(concat)
+ 
   # Define function for transforming to 2263 and calculating distance
   def obtain_distance(x, y, a, b):
-
     t = Transformer.from_crs(4326,2263)
     new_x, new_y = t.transform(x,y)
     new_a, new_b = t.transform(a,b)
@@ -99,26 +116,44 @@ def main(sc):
   # Create udf
   udf_dist = F.udf(obtain_distance)
 
+  # Calculate distances using df of unique origin-destination pairs
+  dist_df = df3.withColumn('origin-dest', merge_od(F.col('placekey'), F.col('key'))) \
+    .select('origin-dest', 'placekey', 'key') \
+    .distinct() \
+    .join(df_supermarkets, F.col('placekey')==df_supermarkets['naics_placekey'], how='inner') \
+    .join(df_cbgs, F.col('key')==df_cbgs['cbg_fips'], how='inner') \
+    .withColumn('dist', udf_dist(F.col('supermarkets_x'), F.col('supermarkets_y'), F.col('cbg_x'), F.col('cbg_y'))) \
+    .select('origin-dest','dist') \
+    .cache()
+
+  # Create dictionary of distances per origin-destination pair
+  # Use dictionary to map original df
+
+  dist_dict = {x['origin-dest']: x['dist'] for x in dist_df.collect()}
+  mapping_expr = F.create_map([F.lit(x) for x in chain(*dist_dict.items())])
+
+
 
   # STEP 5: GET AVERAGE DISTANCE AND FINAL OUTPUT
 
-  # Apply udf to cbg and supermarket coordinates
   # Get total visitor count and distance per cbg
   # Compute average distance per cbg
   # Pivot by date
   # Sort by cbg_fips
 
-  months = ['2019-03','2019-10','2020-03','2020-10']
-
-  df5 = df4.withColumn('dist', udf_dist(F.col('supermarkets_x'), F.col('supermarkets_y'), F.col('cbg_x'), F.col('cbg_y'))) \
-    .select('cbg_fips','date','visitor_count',
-                   F.col('dist').cast(FloatType())) \
+  df4 = df3.withColumn('origin-dest', merge_od(F.col('placekey'), F.col('key'))) \
+    .withColumn('dist', mapping_expr.getItem(F.col('origin-dest'))) \
+    .na.drop() \
+    .select(F.col('key').alias('cbg_fips'),
+            'date',
+            F.col('sum(value)').alias('visitor_count'),
+            F.col('dist').cast(FloatType())) \
     .groupBy('cbg_fips','date').sum('visitor_count','dist') \
-    .withColumn('avg_dist', F.round(F.col('sum(dist)')/F.col('sum(visitor_count)'),2)) \
-    .groupBy('cbg_fips').pivot('date',months).sum('avg_dist') \
+    .withColumn('avg_dist', F.round(F.col('sum(dist)')/F.col('sum(visitor_count)'), 2)) \
+    .groupBy('cbg_fips').pivot('date').max('avg_dist') \
     .sort('cbg_fips') \
     .write.option('header','true') \
-    .csv(sys.argv[1] if len(sys.argv)>1 else 'output')
+    .csv(sys.argv[1] if len(sys.argv)>1 else 'output_ec2')
 
 if __name__=="__main__":
   sc = SparkContext()
